1c0c3d0b59e51f1518bafaa36207c27e8fa45ec9
Marcel Hermanns <marcel.hermanns.external@jungheinrich.de>
2026-09-13 14:38:53 +0000

    Removed obsolete

--- src/zabap_prc_fiori_2023/zabap_prc_fiori_2023_t2/zabap_prc_fiori_t2_old/zui_prc_procmsg.wapa.component.js
+++
@@ -1,2 +0,0 @@
-sap.ui.define(["sap/fe/core/AppComponent"],function(e){"use strict";return e.extend("de.jh.cross.prc.zuiprcprocessedmessage.Component",{metadata:{manifest:"json"}})});                                                                                        
-//# sourceMappingURL=Component.js.map                                                                                                                                                                                                                          
--- src/zabap_prc_fiori_2023/zabap_prc_fiori_2023_t2/zabap_prc_fiori_t2_old/zuiprc_proc_obj.wapa.component.js
+++
@@ -1,2 +0,0 @@
-sap.ui.define(["sap/fe/core/AppComponent"],function(e){"use strict";return e.extend("de.jh.cross.prc.zuiprcprocessobject.Component",{metadata:{manifest:"json"}})});                                                                                           
-//# sourceMappingURL=Component.js.map                                                                                                                                                                                                                          
